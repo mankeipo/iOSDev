8a110012a227b744436c65da48261d6c0eafb024
Man Kei <pomankei66666@gmail.com>
2024-09-17 07:00:38 -0400

    Initial Commit

---
+++ iOSDev/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSDev
+//
+//  Created by Alan Po on 9/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ByeView()
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSDev/iOSDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDevApp.swift
+//  iOSDev
+//
+//  Created by Alan Po on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
